3886f33b45c9e6718b5d62107abf79d590e406a7
BSH <gamaugi11@gmail.com>
2024-07-13 00:03:00 +0900

    init

---
+++ src/test/java/com/diary_server/DiaryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diary_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
