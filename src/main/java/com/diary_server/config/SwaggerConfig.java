152ca61d981fac863fdbdea26bd4982339d64ede
BSH <gamaugi11@gmail.com>
2024-07-18 12:33:31 +0900

    feat: swagger 추가
    - swagger에 jwt 인증 기능 추가
    - webConfig에서 cors설정 추가

---
+++ src/main/java/com/diary_server/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.diary_server.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile({"local", "dev"})
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Diary Server Rest API Specifications")
+                .description("Specification")
+                .version("1.0.0");
+    }
+
+}
